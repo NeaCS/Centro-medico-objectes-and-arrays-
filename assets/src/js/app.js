895fc9d8c22357bd9136d0c4d25031e493b7cf8c
NeaCS <naldi.castro@gmail.com>
2022-02-15 22:53:20 -0300

    creating objects with patients information

---
+++ assets/src/js/app.js
@@ -0,0 +1,17 @@
+let pacientesRadiologia = [{
+    paciente = "FRANCISCA ROJAS",
+    rut = "9878782-1",
+    especialista = "IGNACIO SCHULZ",
+    hora = "11:00",
+    prevision = "FONASA"
+},
+
+{
+    paciente = "PAMELA ESTRADA",
+    rut = "15345241-3",
+    especialista = "IGNACIO SCHULZ",
+    hora = "11:00",
+    prevision = "FONASA"
+}
+
+]
