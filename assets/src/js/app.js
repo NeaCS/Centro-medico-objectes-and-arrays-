f94fb9bcfab7f604d5087477b6aede412c446722
NeaCS <naldi.castro@gmail.com>
2022-02-15 23:22:41 -0300

    data radiología & traumatología completed

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,4 +1,5 @@
-let pacientesRadiologia = [{
+let pacientesRadiologia = [
+    {
     paciente = "FRANCISCA ROJAS",
     rut = "9878782-1",
     especialista = "IGNACIO SCHULZ",
@@ -9,9 +10,91 @@ let pacientesRadiologia = [{
 {
     paciente = "PAMELA ESTRADA",
     rut = "15345241-3",
-    especialista = "IGNACIO SCHULZ",
-    hora = "11:00",
-    prevision = "FONASA"
-}
+    especialista = "FEDERICO SUBERCASEAUX",
+    hora = "11:30",
+    prevision = "ISAPRE"
+},
 
-]
+{
+    paciente = "ARMANDO LUNA",
+    rut = "16445345-9",
+    especialista = "FERNANDO WURTHZ",
+    hora = "3:00",
+    prevision = "ISAPRE"
+}, 
+
+{
+    paciente = "MANUEL GODOY",
+    rut = "17666419-0",
+    especialista = "ANA MARIA GODOY",
+    hora = "3:30",
+    prevision = "FONASA"
+}, 
+
+{
+    paciente = "RAMON ULLOA",
+    rut = "14989389-K",
+    especialista = "PATRICIA SUAZO",
+    hora = "4:00",
+    prevision = "FONASA"
+}, 
+];
+
+let pacientesTraumatologia = [
+    {
+    paciente = "PAULA SANCHEZ",
+    rut = "15554774-5",
+    especialista = "MARIA PAZ ALTUZARRA",
+    hora = "8:00",
+    prevision = "FONASA"
+},
+
+{
+    paciente = "ANGÉLICA NAVAS",
+    rut = "15444147-9",
+    especialista = "RAUL ARAYA",
+    hora = "10:00",
+    prevision = "ISAPRE"
+},
+
+{
+    paciente = "ANA KLAPP",
+    rut = "17879423-9",
+    especialista = "MARIA ARRIAGADA",
+    hora = "10:30",
+    prevision = "ISAPRE"
+}, 
+
+{
+    paciente = "FELIPE MARDONES",
+    rut = "1547423-6",
+    especialista = "ALEJANDRO BADILLA",
+    hora = "11:00",
+    prevision = "ISAPRE"
+}, 
+
+{
+    paciente = "DIEGO MARRE",
+    rut = "16554741-K",
+    especialista = "CECILIA BUDNIK",
+    hora = "11:30",
+    prevision = "FONASA"
+}, 
+
+{
+    paciente = "CECILIA MENDEZ",
+    rut = "9747535-8",
+    especialista = "ARTURO CAVAGNARO",
+    hora = "12:00",
+    prevision = "ISAPRE"
+}, 
+
+{
+    paciente = "MARCIAL SUAZO",
+    rut = "11254785-5",
+    especialista = "ANDRES KANACRI",
+    hora = "12:30",
+    prevision = "ISAPRE"
+}, 
+];
+
